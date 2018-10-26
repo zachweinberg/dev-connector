6c9500328de907e48bcbd57e8c4294e58b862a56
Zach James <zachjames@Zachs-MBP.hsd1.ma.comcast.net>
2018-10-26 19:02:27 -0400

    Added login logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load User Model
+const User = require('../../models/User');
 
 // @route   GET /api/users/test
 // @desc    Tests users route
@@ -7,5 +12,70 @@ const router = express.Router();
 router.get('/test', (req, res) => {
   res.json({ msg: 'Users works!' });
 });
+
+// @route   GET /api/users/register
+// @desc    Register a user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: 'Email already exists' });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200',  //Size
+          r: 'pg',   //Rating
+          d: 'mm'    //Default
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        })
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json( user ))
+              .catch(err => console.log(err))
+          })
+        })
+      }
+    })
+});
+
+// @route   GET /api/users/login
+// @desc    Login user and return a JWT
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find the user by email
+  User.findOne({ email })
+    .then(user => {
+      //Check for user
+      if (!user) {
+        return res.status(404).json({ email: 'User not found' });
+      }
+
+      //Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            res.json({ msg: 'Success' });
+          } else {
+            return res.status(400).json({ password: 'Password invalid' });
+          }
+        })
+    })
+});
+
+
+
 
 module.exports = router;
